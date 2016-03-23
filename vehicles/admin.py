aa8f0caa35dc547deaac97ff5d0ba4732d700896
Dion Gerritsen <dion@stamkracht.com>
2016-03-23 08:41:57 +0100

    Vehicle admin

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from vehicles.models import Vehicle, VehicleSignal
 
 
@@ -11,5 +12,14 @@ class VehicleAdmin(admin.ModelAdmin):
     inlines = [
         VehicleSignalAdmin,
     ]
+
+    list_display = (
+        'license',
+        'owner',
+        'get_signals',
+    )
+
+    def get_signals(self, obj):
+        return mark_safe("<br/>".join(["%s voor %s"%(s.code, s.organization) for s in obj.signals.all()]))
 
 admin.site.register(Vehicle, VehicleAdmin)
